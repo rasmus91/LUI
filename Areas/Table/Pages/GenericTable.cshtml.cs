29eb4f7710de107add58d258bef86d3d2ba50c52
Rasmus Lindegaard <rasmusnielsen91@gmail.com>
2020-03-06 22:37:39 +0100

    Initial Commit

---
+++ Areas/Table/Pages/GenericTable.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LUI.Areas.Table.Pages
+{
+    public class GenericTable : PageModel
+    {
+        
+        public string Name { get { return "GenericTableName"; } }
+
+    }
+}
